82daa4117d2439f8c10b60d4001db036e94d322a
Chinmay Kulkarni <chinmaykulkarni8@gmail.com>
2020-01-16 11:51:44 +0530

    PlayFairCipher string modification complete

--- src/com/chinmay/PlayFairCipher.java
+++ src/com/chinmay/PlayFairCipher.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * A program to encrypt a message using PlayFair Cipher
+ *
+ * @author grandolf49
+ */
 public class PlayFairCipher {
 
     private static final int SIZE = 5;
@@ -21,6 +26,9 @@ public class PlayFairCipher {
         System.out.println("Encrypted string is: " + encryptedString);
     }
 
+    /**
+     * A function to encrypt the message using key
+     */
     private static String playFairCipherEncryption(String message, String keyword) {
         StringBuilder encryptedString = new StringBuilder();
         char[][] matrix = new char[SIZE][SIZE];
@@ -53,10 +61,39 @@ public class PlayFairCipher {
                 key_idx++;
             }
         }
-
         System.out.println("Cipher Matrix:");
         displayMatrix(matrix);
+
+        // Modify the message so its length is even and no repeated letters in group of 2
+        String newMessage = constructMessage(message);
+        System.out.println("New message: " + newMessage);
+
         return encryptedString.toString();
+    }
+
+    /**
+     * A function to make sure that there are no repeated letters in group of 2 and the length of the message is even
+     */
+    private static String constructMessage(String message) {
+        StringBuilder stringBuilder = new StringBuilder(message);
+
+        for (int i = 0; i < stringBuilder.length(); i += 2) {
+            int next_idx = i + 1;
+            if (next_idx == stringBuilder.length()) {
+                if (stringBuilder.charAt(i) == 'x')
+                    stringBuilder.append('y');
+                else
+                    stringBuilder.append('x');
+            } else {
+                if (stringBuilder.charAt(i) == stringBuilder.charAt(next_idx)) {
+                    if (stringBuilder.charAt(i) == 'x')
+                        stringBuilder.insert(next_idx, 'y');
+                    else
+                        stringBuilder.insert(next_idx, 'x');
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 
     private static void displayMatrix(char[][] matrix) {
