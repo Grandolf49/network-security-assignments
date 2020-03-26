f0e951099f0cfad6751eb140bd785c99bfcb32b8
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-03-26 18:52:52 +0530

    Diffie Hellman Key Exchange completed

---
+++ src/com/chinmay/dhke/DiffieHellmanConstants.java
@@ -0,0 +1,13 @@
+package com.chinmay.dhke;
+
+/**
+ * A class to store all the constants used for Diffie Hellman Key Exchange
+ *
+ * @author grandolf49
+ */
+public class DiffieHellmanConstants {
+
+    public static final int PUBLIC_P = 23;
+    public static final int PUBLIC_G = 9;
+
+}
